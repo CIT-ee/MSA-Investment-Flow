d902681a0571726bf178486dc9f6c19a4a06dc7d
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2017-11-13 00:42:40 +0000

    add logic for adding msa data to the locations

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,6 +6,7 @@ sys.path.insert(0, os.environ['PROJECT_PATH'])
 import subprocess, pandas as pd
 
 from config.resources import path_to
+from src.data.msa_mapper.map_loc_to_msa import MSAMapper
 
 def setup_raw_data_dump():
     subprocess.call('./src/data/setup_data_dump.sh')
@@ -38,25 +39,54 @@ def map_investor_investee(path_to_dest):
                                         funding_rounds_df, how='left', 
                                         on='funding_round_uuid', 
                                         suffixes=('_investors', '_investees'))
-    #  print(investment_investor_bridge.head())
 
     print('Investor - investee bridge formed! Dumping data to {}'.format(path_to_dest))
     investor_investee_bridge.to_csv(path_to_dest, index=False, encoding='latin1')
 
+def _add_msa_data(source_df, suffix):
+    msa_mapper_investor = MSAMapper(source_df.fillna(''))
+    source_df = msa_mapper_investor.map_data()
+    column_renamer = lambda x: x.lower() + suffix if suffix not in x else x
+    source_df.rename(column_renamer, axis=1, inplace=True)
+    return source_df
+
 def filter_data(path_to_src, path_to_dest, investment_type, investor_type, 
-                country_code='USA'):
+                add_msa_data, country_code='USA'):
     print('\nPreparing to filter out non relevant data. Please wait..')
 
-    #  filter out non relevant investor and investment types, and countries
+    #  filter out non US countries
     master_df = pd.read_csv(path_to_src, encoding='latin1')
 
     master_df = master_df[master_df['country_code_investees'] == country_code]
+    master_df = master_df[master_df['country_code_investors'] == country_code]
+
+    #  filter out non relevant investor and investment types
     filtered_by_investment_df = master_df[master_df['investor_type'] == investor_type]
     filtered_by_investor_df = master_df[master_df['investment_type'] == investment_type]
     filtered_df = pd.merge(filtered_by_investor_df, filtered_by_investment_df, 
                             how='inner', on=master_df.columns.tolist())
 
-    print('\nFiltering out non relevant data completed! Dumping data to {}'.format(path_to_dest))
+    print('Filtering out non relevant data completed!\n')
+
+    if add_msa_data:
+        investor_loc_df = filtered_df[ ['city_investors', 'state_code_investors', 'country_code_investors'] ]
+        investee_loc_df = filtered_df[ ['city_investees', 'state_code_investees', 'country_code_investees'] ]
+        
+        #  map investor locations to msa
+        print('\nPreparing to add msa data to investor locations')
+        investor_loc_df = _add_msa_data(investor_loc_df, '_investors')
+        common_columns = investor_loc_df.columns.tolist()[:3]
+        filtered_df = pd.merge(filtered_df, investor_loc_df, how='inner', 
+                                on=common_columns).drop_duplicates()
+
+        #  map investee locations to msa
+        print('\nPreparing to add msa data to investee locations')
+        investee_loc_df = _add_msa_data(investee_loc_df, '_investees')
+        common_columns = investee_loc_df.columns.tolist()[:3]
+        filtered_df = pd.merge(filtered_df, investee_loc_df, how='inner', 
+                                on=common_columns).drop_duplicates()
+
+    print('Dumping data to {}'.format(path_to_dest))
     filtered_df.to_csv(path_to_dest, index=False, encoding='latin1')
 
 if __name__ == '__main__':
@@ -65,6 +95,7 @@ if __name__ == '__main__':
     parser.add_argument('--op', default='invst2org', help='operation to perform')
     parser.add_argument('--investment', default='series_a', help='type of investment to focus on')
     parser.add_argument('--investor', default='angel_group', help='type of investor to focus on')
+    parser.add_argument('--map_msa', action='store_true', help='flag: add msa data or not')
     
     args = parser.parse_args()
 
@@ -79,4 +110,4 @@ if __name__ == '__main__':
         path_to_dump = path_to['filtered_data'].format(args.investment, args.investor)
         assert os.path.exists(path_to_data), \
             'Please build investor - investee master data first!'
-        filter_data(path_to_data, path_to_dump, args.investment, args.investor)
+        filter_data(path_to_data, path_to_dump, args.investment, args.investor, args.map_msa)
