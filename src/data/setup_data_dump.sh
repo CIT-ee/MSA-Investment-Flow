31757edb2cad3a5f2153f0bf63e69f15f594beed
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2017-12-23 20:35:35 +0000

    update: remove hardcoded url to allow more flexibility

--- src/data/setup_data_dump.sh
+++ src/data/setup_data_dump.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-path_to_data_dump=${DATA_PATH}/raw
+path_to_data_dump=${DATA_PATH}/raw/${1}
+[[ ! -d ${path_to_data_dump} ]] && mkdir -p ${path_to_data_dump}
 rm -rf ${path_to_data_dump}/*
 echo -e '\nSetting up data dump at '${path_to_data_dump}'\n'
-wget https://api.crunchbase.com/v3.1/csv_export/csv_export.tar.gz?user_key=${CRUNCHBASE_API_KEY} -O ${path_to_data_dump}/csv_export.tar.gz
-tar -xzvf ${path_to_data_dump}/csv_export.tar.gz -C $path_to_data_dump/
-rm ${path_to_data_dump}/csv_export.tar.gz 
+wget https://api.crunchbase.com/v3.1/${1}/${1}.tar.gz?user_key=${CRUNCHBASE_API_KEY} -O ${path_to_data_dump}/${1}.tar.gz
+tar -xzvf ${path_to_data_dump}/${1}.tar.gz -C $path_to_data_dump/
+rm ${path_to_data_dump}/${1}.tar.gz 
 echo -e '\nSetting up data completed!'
