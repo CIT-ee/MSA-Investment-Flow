2f6136b623ff262e0185bf513cbaba1a93aeb205
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2018-01-04 03:01:16 +0000

    add batch support for adding msa data; update msa-mapping submodule

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -26,9 +26,8 @@ def _assert_paths(path_to_source, path_to_dest):
 def setup_raw_data_dump(fname):
     subprocess.call( [ './src/data/setup_data_dump.sh', fname ] )
 
-def filter_investments(path_to_source, nb_years):
+def _filter_investments(source_df, nb_years=3):
     print('\nPreparing to filter dataframe. Please wait ..')
-    source_df = pd.read_csv(path_to_source, encoding='latin1')
 
     #  filter out data before `nb_years` from today
     curr_year = datetime.now().year + 1
@@ -41,7 +40,9 @@ def filter_investments(path_to_source, nb_years):
     print('Filtering dataframe completed!\n')
     return source_df[ source_df['company_name'] != 'Distributed ID' ].reset_index()
 
-def build_investment_flow_df(source_df, path_to_dest, fields):
+def build_investment_flow_df(path_to_source, path_to_dest, fields):
+    source_df = _filter_investments(pd.read_csv(path_to_source, encoding='latin1'))
+
     #  set the url template
     url_template = "https://api.crunchbase.com/v3.1/funding-rounds/{uuid}/{relationship}?user_key={api_key}"
     
@@ -66,6 +67,24 @@ def build_investment_flow_df(source_df, path_to_dest, fields):
     dest_df.rename(col_renamer, axis='columns', inplace=True)
 
     print('Building investment dataframe completed!. Dumping data to {}\n'.format(path_to_dest))
+    dest_df.to_csv(path_to_dest, encoding='utf-8', index=False)
+
+def add_msa_data(path_to_source, path_to_dest, src_loc_fields, dest_loc_fields, data_format):
+    source_df = pd.read_csv(path_to_source, encoding='latin1')
+
+    #  filter the dataframe if its related to funding rounds
+    source_df = _filter_investments(source_df) if 'funding_rounds' in path_to_source else source_df
+
+    print('\nPreparing to add MSA data to the data frame in question. Please wait ..')
+    src_loc_df = source_df[ src_loc_fields ]
+    msa_mapping_client = MSAMapper(src_loc_df.fillna(''))
+    dest_loc_df = msa_mapping_client.map_data(data_format)
+    
+    #  TODO: make this join more robust
+    pdb.set_trace()
+    dest_df = pd.concat([ source_df, dest_loc_df[ dest_loc_fields ] ], axis=1)
+
+    print('Adding MSA data to dataframe completed! Dumping data to {}'.format(path_to_dest))
     dest_df.to_csv(path_to_dest, encoding='utf-8', index=False)
 
 def batchify_data(path_to_source, path_to_dump, batch_size):
@@ -104,6 +123,26 @@ if __name__ == '__main__':
 
         batchify_data(path_to_source, path_to_dest, args.batch_size) 
 
+    elif args.op == 'add_msa':
+        assert args.node is not None, 'Please choose node to operate on first!'
+
+        #  set the paths to source and destination
+        if args.batch is None:
+            path_to_source = path_to['csv_export'].format(args.node)
+            path_to_dest = path_to['with_msa_csv'].format(node=args.node)
+        else:
+            path_to_source = path_to['batch_csv'].format(node=args.node, idx=args.batch)
+            path_to_dest = path_to['with_msa_batch_csv'].format(node=args.node, idx=args.batch)
+
+        _assert_paths(path_to_source, path_to_dest)
+
+        if args.node == 'funding_rounds':
+            src_loc_fields = [ 'city', 'state_code', 'country_code' ]
+            dest_loc_fields = [ 'MSA_NAME', 'MSA_CODE' ]
+            data_format = 'address'
+
+        add_msa_data(path_to_source, path_to_dest, src_loc_fields, dest_loc_fields, data_format)
+
     elif args.op == 'map_investments':
         assert args.node is not None, 'Please choose node to operate on first!'
 
@@ -117,5 +156,4 @@ if __name__ == '__main__':
 
         _assert_paths(path_to_source, path_to_dest)
 
-        source_df = filter_investments(path_to_source, 3)
-        build_investment_flow_df(source_df, path_to_dest, fields['funding_rounds'])
+        build_investment_flow_df(path_to_source, path_to_dest, fields['funding_rounds'])
